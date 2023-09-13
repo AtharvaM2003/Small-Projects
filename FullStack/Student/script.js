9440f507093e2fda51d625288b3f9fdd4c4ad478
AtharvaM2003 <atharva2mundhe@gmail.com>
2023-09-13 20:33:42 +0530

    done

---
+++ FullStack/Student/script.js
@@ -0,0 +1,11 @@
+function msg(){
+    let na=document.f1.na.value;
+    let roll=document.f1.roll.value;
+    let add=document.f1.add.value;
+
+    if(na==""||roll==""||add==""){
+    alert("Enter Details") }
+    else{
+        alert("Welcome "+na + " Registration Successfully!!");
+    }
+}
