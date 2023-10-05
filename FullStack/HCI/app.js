845fec72d1c5f5eef70876ed4826e2b3e2bac7e1
AtharvaM2003 <atharva2mundhe@gmail.com>
2023-10-05 20:06:17 +0530

    Done HCI

--- FullStack/HCI/app.js
+++ FullStack/HCI/app.js
@@ -1,50 +1,93 @@
-// Define a collection of Indian recipes
-const indianRecipes = [
-  {
-    name: "Butter Chicken",
-    instructions: "1. Marinate chicken in yogurt and spices.\n2. Cook chicken in a tomato-based sauce with butter and cream."
+const content = document.querySelector(".content");
+const btn = document.querySelector(".talk");
+
+const recipes = {
+  "butter chicken": {
+    introduction: "Let's make delicious Butter Chicken today.",
+    steps: [
+      "Step 1: Marinate chicken with yogurt, spices, and lemon juice for 2 hours.",
+      "Step 2: Heat oil in a pan and sauté onions, garlic, and ginger until fragrant.",
+      "Step 3: Add tomato puree and cook until the oil separates.",
+      "Step 4: Add marinated chicken and cook until it's no longer pink.",
+      "Step 5: Stir in cream, butter, and garam masala. Simmer for 10 minutes.",
+      "Step 6: Garnish with cilantro and serve hot with naan or rice.",
+    ],
   },
-  {
-    name: "Chicken Biryani",
-    instructions: "1. Cook marinated chicken with rice, spices, and saffron.\n2. Layer rice and chicken, then steam until done."
+  "biryani": {
+    introduction: "How about preparing a flavorful Biryani?",
+    steps: [
+      "Step 1: Soak basmati rice for 30 minutes.",
+      "Step 2: Cook rice with spices and saffron until half-cooked.",
+      "Step 3: Layer rice with marinated meat and cook in a covered pot until done.",
+      "Step 4: Garnish with fried onions and serve hot.",
+    ],
   },
-  // Add more Indian recipes as needed
-];
+  "paneer tikka": {
+    introduction: "Let's cook some Paneer Tikka.",
+    steps: [
+      "Step 1: Marinate paneer cubes with yogurt and spices.",
+      "Step 2: Thread the paneer onto skewers and grill until charred.",
+      "Step 3: Serve with mint chutney and lemon wedges.",
+    ],
+  },
+  "chole bhature": {
+    introduction: "How about Chole Bhature for lunch?",
+    steps: [
+      "Step 1: Soak chickpeas overnight and cook with spices.",
+      "Step 2: Make a spicy tomato gravy with onions and spices.",
+      "Step 3: Serve hot with deep-fried bhature.",
+    ],
+  },
+  "samosa": {
+    introduction: "Let's make crispy Samosas.",
+    steps: [
+      "Step 1: Prepare a spiced potato and pea filling.",
+      "Step 2: Make samosa dough and shape it into triangles.",
+      "Step 3: Fill each triangle with the potato mixture and deep-fry until golden.",
+    ],
+  },
+};
 
-// Rest of the code remains the same
+const hello = ["Hello, how can I assist you with Indian recipes?"];
 
-// Function to fetch a recipe based on user input
-function fetchRecipe(query) {
-  // Replace this with actual logic to fetch a recipe based on the user's input
-  // You might use a loop to search through the array of recipes
-  // For simplicity, let's assume the user provides the recipe name directly
-  const lowerCaseQuery = query.toLowerCase();
-  const foundRecipe = indianRecipes.find(recipe => lowerCaseQuery.includes(recipe.name.toLowerCase()));
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
 
-  return foundRecipe;
-}
+recognition.onresult = function (event) {
+  const current = event.resultIndex;
+  const transcript = event.results[current][0].transcript;
+  content.textContent = transcript;
+  readOutLoud(transcript);
+};
 
-// Modify the readOutLoud function to handle Indian recipes
+btn.addEventListener("click", () => {
+  recognition.start();
+});
+
 function readOutLoud(message) {
   const speech = new SpeechSynthesisUtterance();
   speech.text = "I am not able to understand, sir";
 
-  if (message.includes("Hello.")) {
-    const finalText = "Hello! How can I assist you with recipes today?";
+  if (message.includes("Hey") || message.includes("Hello")) {
+    const finalText = hello[0];
     speech.text = finalText;
-  } else if (message.includes("recipe")) {
-    const recipeName = extractRecipeName(message);
-    const recipe = fetchRecipe(recipeName);
+  } else {
+    const recipeName = Object.keys(recipes).find((name) =>
+      message.toLowerCase().includes(name)
+    );
 
-    if (recipe) {
-      const finalText = `Here is the recipe for ${recipe.name}:\n${recipe.instructions}`;
+    if (recipeName) {
+      const recipe = recipes[recipeName];
+      const finalText = `${recipe.introduction} Here are the steps to make ${recipeName}: ${recipe.steps.join(
+        " "
+      )}`;
       speech.text = finalText;
     } else {
-      speech.text = "I couldn't find that recipe in my collection.";
+      const finalText = "Sorry, I am not able to assist with that recipe.";
+      speech.text = finalText;
     }
   }
-
-  // Add more logic for different recipe-related queries as needed
 
   speech.volume = 1;
   speech.rate = 1.1;
@@ -52,5 +95,3 @@ function readOutLoud(message) {
 
   window.speechSynthesis.speak(speech);
 }
-
-// Rest of the code remains the same
